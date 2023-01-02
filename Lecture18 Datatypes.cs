fb14938e72bdb1e4f17cd226b80add5a6ee9d2eb
Hardisonm <121726547+Hardisonm@users.noreply.github.com>
2023-01-02 18:46:13 -0500

    Add files via upload

---
+++ Lecture18 Datatypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lecture18_Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Michael";
+
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+            
+            string lowercasemessage = message.ToLower();
+            
+            
+            Console.WriteLine("My name is " + myname);
+
+            Console.WriteLine(capsMessage);
+
+            Console.WriteLine(lowercasemessage);
+
+            Console.Read();
+        }
+    }
+}
